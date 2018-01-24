30a712da653a9cd4960d0fb65c7b2d3089a8ed04
Zack Haigh <zack592@gmail.com>
2018-01-24 08:57:03 -0500

    Add IE support

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import TourDateList from './components/tour-date-list';
 import Header from './components/header';
 import Footer from './components/footer';
-
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
 
 class App extends Component {
   constructor(props) {
@@ -12,11 +13,10 @@ class App extends Component {
       tourDates: []
     };
 
-      fetch('https://rest.bandsintown.com/artists/Sweater%20Beats/events?app_id=Test')
+    fetch('https://rest.bandsintown.com/artists/Sweater%20Beats/events?app_id=Test')
       .then(response => response.json() )
       .then(tourDates => this.setState({tourDates: tourDates})
-      )
-
+    )
   }
 
   render() {
